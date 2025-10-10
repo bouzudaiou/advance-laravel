21fa3445ba474d7a95587d726d22f7f230244355
yoshinobu k <ysnb5884@gmail.com>
2025-10-10 09:55:53 +0900

    Implement author list display feature with MVC pattern

--- src/app/Http/Controllers/AuthorController.php
+++ src/app/Http/Controllers/AuthorController.php
@@ -5,7 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Author;
 
-class AuthorController extends Controller
+    class AuthorController extends Controller
 {
-    //
+    public function index()
+    {
+        $authors = Author::all();
+        return view('index', ['authors' => $authors]);
+    }
 }
